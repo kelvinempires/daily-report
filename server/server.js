416a3ec32be9ceeac5eda0dd4b2b845815377768
kelvinempire <kelvinewurum@gmail.com>
2025-02-13 17:35:23 +0100

    Remove CORS middleware and allowed origins from server configuration

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
-import cors from 'cors'
 import 'dotenv/config'
 import dbConnection from './db/conn.js';
 import authRouter from "./routes/authRoute.js"
@@ -10,11 +9,9 @@ dbConnection();
 const app = express()
 const PORT = process.env.PORT || 5000
 
-const allowedOrigins = ["http://localhost:5173"];
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({origin: allowedOrigins,credentials: true}))
 
 app.get("/", (req, res) =>{res.send("hello from server")});
 
